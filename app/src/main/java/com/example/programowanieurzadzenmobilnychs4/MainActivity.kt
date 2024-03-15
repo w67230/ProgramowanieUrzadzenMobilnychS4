b1ee5b00844411c3cbc8cca1e92c9d23471ed55a
w67230 <w67230@student.wsiz.edu.pl>
2024-03-15 10:36:09 +0100

    obrazek

--- app/src/main/java/com/example/programowanieurzadzenmobilnychs4/MainActivity.kt
+++ app/src/main/java/com/example/programowanieurzadzenmobilnychs4/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.programowanieurzadzenmobilnychs4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -47,11 +51,42 @@ fun MojTekst(message: String, sMessage: String, wlaczKolorki: Boolean = false, m
 
 }
 
+@Composable
+fun MojTekstImage(message: String, sMessage: String, wlaczKolorki: Boolean = false, modifier: Modifier = Modifier){
+    Box(modifier) {
+        val image = painterResource(R.drawable.androidparty)
+        Image(painter = image,
+            contentDescription = null,
+            modifier = modifier.alpha(0.5F))
+
+        var fColor = Color.Unspecified
+        var sColor = Color.Unspecified
+        if(wlaczKolorki){
+            fColor = Color.Blue
+            sColor = Color.DarkGray
+        }
+        Text(text = message,
+            fontSize = 56.sp,
+            lineHeight = 70.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier.align(Alignment.Center),
+            color = fColor)
+
+        Text(text = sMessage,
+            fontSize = 32.sp,
+            modifier = Modifier.align(Alignment.BottomEnd),
+            color = sColor)
+
+    }
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun MojTekstPreview() {
     ProgramowanieUrzadzenMobilnychS4Theme {
-        MojTekst("Najlepszego Ukasz", "Tak to ja", true)
+        MojTekstImage("Najlepszego Ukasz", "Tak to ja", false)
     }
 }
